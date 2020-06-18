2eceba983f16814b648ed143ad29789757811802
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-06-18 09:33:31 +0800

    Added distance to exercises array in workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,13 +3,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: { type: String,  required: [true, 'Exercise type required'] },
+      name: { type: String,  required: [true, 'Exercise name required'] },
+      distance: { type: Number },
+      duration: { type: Number, required: [true, 'Exercise duration required'] },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
     },
-    exercises: [{type: String, name: String, duration: Number, weight: Number, reps: Number, sets: Number}]
-    
-})
+  ],
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
